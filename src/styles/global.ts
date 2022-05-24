ccb13464934871dc4ec1baa9c768b91611a17f71
Rodrigo Machado Mena <rodrigo_mena_corebiz@whirlpool.com>
2022-05-24 15:02:00 -0300

    Estilizando modal #1

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,6 +12,9 @@ export const GlobalStyle = createGlobalStyle`
 
         --text-title: #363f5f;
         --text-body: #969cb3;
+
+        --input-border: #d7d7d7;
+        --input-background: #e7e9ee;
     }
 
     * {
@@ -75,5 +78,18 @@ export const GlobalStyle = createGlobalStyle`
         padding: 3rem;
         position: relative;
         border-radius: 0.25rem;
+    }
+
+    .react-modal-close {
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
+        transition: filter 0.2s;
+
+        &:hover {
+            filter: brightness(0.8);
+        }
     }
 `
